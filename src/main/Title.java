27a6946f568c1bec2d549f6d8d3e7e36f9f52663
AndreasFlueckiger <162303766+AndreasFlueckiger@users.noreply.github.com>
2025-06-07 17:34:25 +0200

    Create Title.java

---
+++ src/main/Title.java
@@ -0,0 +1,40 @@
+package main;
+
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+import battleship.battleshipConfiguration;
+
+@SuppressWarnings("serial")
+public class Title extends JPanel{
+
+	JLabel titleLabel = new JLabel();
+	
+	public Title(String title) {
+		
+		setLayout(null);
+		setBounds(0, 0, K.LARG_DEFAULT, 50);
+		setBackground(Color.CYAN);
+		setOpaque(false);
+		
+		titleLabel.setText(title);
+		titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		titleLabel.setVerticalAlignment(SwingConstants.CENTER);
+		titleLabel.setBounds(0, 5, 1024, 40);
+		titleLabel.setFont(new Font("SansSerif", Font.BOLD, 24));
+		titleLabel.setForeground(new Color(0, 0, 0));
+		
+		add(titleLabel);
+		
+	}
+	
+	public void setText(String text) {
+		titleLabel.setText(text);
+		repaint();
+	}
+	
+}
