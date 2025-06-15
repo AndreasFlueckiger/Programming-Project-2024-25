a727a1b94e104af80d7a60a155b6a96a7fffd48c
AndreasFlueckiger <162303766+AndreasFlueckiger@users.noreply.github.com>
2025-06-15 09:52:21 +0200

    Update Cruiser.java

--- src/main/logic/ships/Cruiser.java
+++ src/main/logic/ships/Cruiser.java
@@ -1,5 +1,34 @@
 package main.logic.ships;
 
-public class Cruiser {
-    
+import main.battleship.BattleshipConfiguration
+
+
+@SuppressWarnings("serial")
+public class Cruiser extends Ship {
+
+	private final int CRUISER_SIZE = 4;
+	private static final int CRUISER_POSITION = 3;
+	
+	private static Cruiser cruiser;
+	
+	public static Cruiser getCruiser() {
+		if(cruiser == null) {
+			cruiser = new Cruiser(OFFSET_X, OFFSET_Y*CRUISER_POSITION);
+		}
+		
+		return cruiser;
+	}
+	
+	public void selfDestroy() {
+		cruiser = null;
+	}
+
+	private Cruiser(int x, int y) {
+
+		setBounds(x, y, BattleshipConfiguration.SQUARE_SIZE*CRUISER_SIZE, BattleshipConfiguration.SQUARE_SIZE);
+		setOpaque(false);
+
+		super.paintSquares(CRUISER_SIZE);
+	}
+
 }
