6f1fc90f6648051d9ce367dda51875f36092cae1
AndreasFlueckiger <162303766+AndreasFlueckiger@users.noreply.github.com>
2025-06-15 09:53:50 +0200

    Update Destroyer.java

--- src/main/logic/ships/Destroyer.java
+++ src/main/logic/ships/Destroyer.java
@@ -1,5 +1,35 @@
 package main.logic.ships;
 
-public class Destroyer {
-    
+import main.battleship.BattleshipConfiguration
+
+
+@SuppressWarnings("serial")
+public class Destroyer extends Ship {
+
+	private final int DESTROYER_SIZE = 2;
+	private static final int DESTROYER_POSITION = 4;
+	
+	private static Destroyer destroyer;
+	
+	public static Destroyer getDestroyer() {
+		if(destroyer == null) {
+			destroyer = new Destroyer(OFFSET_X, OFFSET_Y*DESTROYER_POSITION);
+		}
+		
+		return destroyer;
+	}
+	
+	public void selfDestroy() {
+		destroyer = null;
+	}
+
+
+	private Destroyer(int x, int y) {
+
+		setBounds(x, y, BattleshipConfiguration.SQUARE_SIZE*DESTROYER_SIZE, BattleshipConfiguration.SQUARE_SIZE);
+		setOpaque(false);
+
+		super.paintSquares(DESTROYER_SIZE);
+	}
+
 }
