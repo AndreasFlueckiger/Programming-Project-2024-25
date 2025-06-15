ecc3ff62723776a19db482f4450df15225f1f948
AndreasFlueckiger <162303766+AndreasFlueckiger@users.noreply.github.com>
2025-06-15 10:00:00 +0200

    Update Submarine.java

--- src/main/logic/ships/Submarine.java
+++ src/main/logic/ships/Submarine.java
@@ -1,5 +1,35 @@
 package main.logic.ships;
 
-public class Submarine {
-    
+import main.battleship.BattleshipConfiguration;
+
+
+@SuppressWarnings("serial")
+public class Submarine extends Ship {
+
+	private final int SUBMARINE_SIZE = 1;
+	private static final int SUBMARINE_POSITION = 5;
+	
+	private static Submarine submarine;
+	
+	public static Submarine getSubmarine() {
+		if(submarine == null) {
+			submarine = new Submarine(OFFSET_X, OFFSET_Y*SUBMARINE_POSITION);
+		}
+		
+		return submarine;
+	}
+	
+	public void selfDestroy() {
+		submarine = null;
+	}
+
+
+	private Submarine(int x, int y) {
+
+		setBounds(x, y, BattleshipConfiguration.SQUARE_SIZE*SUBMARINE_SIZE, BattleshipConfiguration.SQUARE_SIZE);
+		setOpaque(false);
+
+		super.paintSquares(SUBMARINE_SIZE);
+	}
+
 }
