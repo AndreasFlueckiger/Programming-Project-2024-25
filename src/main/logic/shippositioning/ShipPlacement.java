a2ad0deca119d5c65e753a2b137deb2c83b9e890
AndreasFlueckiger <162303766+AndreasFlueckiger@users.noreply.github.com>
2025-06-15 09:44:52 +0200

    Update ShipPlacement.java

--- src/main/logic/shippositioning/ShipPlacement.java
+++ src/main/logic/shippositioning/ShipPlacement.java
@@ -1,5 +1,25 @@
 package main.logic.shippositioning;
 
-public class ShipPlacement {
-    
+import javax.swing.JPanel;
+
+import main.battleship.BattleshipConfiguration;
+
+
+@SuppressWarnings("serial")
+public class ShipPlacement extends JPanel{
+	
+	SelectionBoard board = new SelectionBoard();
+
+	public ShipPlacement() {
+
+		setBounds(BattleshipConfiguration.LARG_DEFAULT/3, 0, BattleshipConfiguration.LARG_DEFAULT-(BattleshipConfiguration.LARG_DEFAULT/3), BattleshipConfiguration.ALT_DEFAULT - 150);
+		setLayout(null);
+		setOpaque(false);
+		
+		board.setBounds((this.getBounds().width - board.BOARD_SIZE)/2, (this.getBounds().height - board.BOARD_SIZE)/2, board.BOARD_SIZE, board.BOARD_SIZE);
+		
+		add(board);
+		
+	}
+
 }
