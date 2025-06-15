25376007bd97dc4a79033f33a42ab0aca9033508
AndreasFlueckiger <162303766+AndreasFlueckiger@users.noreply.github.com>
2025-06-15 11:09:40 +0200

    Update Observable.java

--- src/main/rules/designPatterns/Observable.java
+++ src/main/rules/designPatterns/Observable.java
@@ -2,11 +2,8 @@ package main.rules.designPatterns;
 
 public interface Observable {
 
-
 	public void addObserver(Observer o);
-    //I deleted the public before these 3 statements
-	 void addObserver(Observer o);
-     void removeObserver(Observer o);
-     Object get();
+   public  void removeObserver(Observer o);
+  public   Object get();
 	
 }
