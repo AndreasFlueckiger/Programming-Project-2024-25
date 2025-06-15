7f8c005649c0faf5740344a1234810ae29cf3141
AndreasFlueckiger <162303766+AndreasFlueckiger@users.noreply.github.com>
2025-06-15 09:51:09 +0200

    Update Battleship.java

--- src/main/logic/ships/Battleship.java
+++ src/main/logic/ships/Battleship.java
@@ -1,5 +1,34 @@
 package main.logic.ships;
 
-public class Battleship {
-    
+import main.battleship.BattleshipConfiguration
+
+
+@SuppressWarnings("serial")
+public class Battleship extends Ship {
+
+	private static final int BATTLESHIP_SIZE = 5;
+	private static final int BATTLESHIP_POSITION = 2;
+	
+	private static Battleship battleship;
+	
+	public static Battleship getBattleship() {
+		if(battleship == null) {
+			battleship = new Battleship(OFFSET_X, OFFSET_Y*BATTLESHIP_POSITION);
+		}
+		
+		return battleship;
+	}
+	
+	public void selfDestroy() {
+		battleship = null;
+	}
+
+	private Battleship(int x, int y) {
+
+		setBounds(OFFSET_X, OFFSET_Y*BATTLESHIP_POSITION, BattleshipConfiguration.SQUARE_SIZE*BATTLESHIP_SIZE, BattleshipConfiguration.SQUARE_SIZE);
+		setOpaque(false);
+
+		super.paintSquares(BATTLESHIP_SIZE);
+	}
+
 }
